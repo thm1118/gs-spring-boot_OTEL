48acbdc73e7da30468c1ac9ec6784048fd976d77
tiger <tiger-shanghai@outlook.com>
2023-11-30 16:03:22 +0800

    添加子方法

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,12 +3,24 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		String value = genMessage("index");
+		return "消息: " +  value;
+	}
+
+	private String genMessage(String url) {
+		if (Objects.equals(url, "index")) {
+			return "欢迎来到首页";
+		}
+		else {
+			return "欢迎继续浏览!";
+		}
 	}
 
 }
